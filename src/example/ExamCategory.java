811a95984cb9d448a975417ce1fa5c2e2661b451
junxtar <aa1870088@naver.com>
2023-10-19 15:09:24 +0900

    모든 카테고리 리스트 정의

--- src/example/ExamCategory.java
+++ src/example/ExamCategory.java
@@ -4,11 +4,11 @@ import model.Category;
 import java.util.List;
 
 public class ExamCategory {
-   public final static Category PIZZA = new Category("피자", "쫄깃한 도우와 이탈리아 치즈 그리고 수제 소스의 환상적인 궁합");
-   public final static Category TOPPING = new Category("토핑", "베이컨, 치즈, 페퍼로니, 없는게 없어유 ~ ");
-   public final static Category BEVERAGE = new Category("음료", "주인장은 제로 음료도 도입을 했습니다.");
-   public final static Category SAUCE = new Category("소스", "소스를 곁들이면 피자 맛이 두배~");
-   public final static Category SIDE = new Category("사이드", "피자로는 부족하다면 탁월한 선택!");
+   public final static Category PIZZA = new Category("Pizza", "쫄깃한 도우와 이탈리아 치즈 그리고 수제 소스의 환상적인 궁합");
+   public final static Category TOPPING = new Category("Topping", "베이컨, 치즈, 페퍼로니, 없는게 없어유 ~ ");
+   public final static Category BEVERAGE = new Category("Beverage", "주인장은 제로 음료도 도입을 했습니다.");
+   public final static Category SAUCE = new Category("Sauce", "소스를 곁들이면 피자 맛이 두배~");
+   public final static Category SIDE = new Category("Side", "피자로는 부족하다면 탁월한 선택!");
 
    public final static List<Category> CATEGORIES = List.of(PIZZA, TOPPING, BEVERAGE, SAUCE, SIDE);
 }

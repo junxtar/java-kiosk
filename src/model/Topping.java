ce8dd48d22829c34bf7aabc4aca4f1e01855934c
junxtar <aa1870088@naver.com>
2023-10-19 15:11:56 +0900

    feat: Topping 클래스 구현

---
+++ src/model/Topping.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Topping extends Product {
+   public Topping(String categoryName, String categoryInformation, String productName, String productInformation, int price) {
+      super(categoryName, categoryInformation, productName, productInformation, price);
+   }
+}

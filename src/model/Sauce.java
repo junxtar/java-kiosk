7f0734947e02761826556ae4478b87b4a551c6f6
junxtar <aa1870088@naver.com>
2023-10-19 15:11:41 +0900

    feat: Sauce 클래스 구현

---
+++ src/model/Sauce.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Sauce extends Product {
+   public Sauce(String categoryName, String categoryInformation, String productName, String productInformation, int price) {
+      super(categoryName, categoryInformation, productName, productInformation, price);
+   }
+}

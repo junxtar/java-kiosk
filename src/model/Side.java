c8311b2e9d698ef57ec3dba58236f166303585b9
junxtar <aa1870088@naver.com>
2023-10-19 15:11:48 +0900

    feat: Side 클래스 구현

---
+++ src/model/Side.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Side extends Product {
+   public Side(String categoryName, String categoryInformation, String productName, String productInformation, int price) {
+      super(categoryName, categoryInformation, productName, productInformation, price);
+   }
+}

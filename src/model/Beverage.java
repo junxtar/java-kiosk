9f0578fee4e2a4f23d22530d072f5b5dfb7e183c
junxtar <aa1870088@naver.com>
2023-10-19 15:11:08 +0900

    feat: Beverage 클래스 구현

---
+++ src/model/Beverage.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Beverage extends Product {
+   public Beverage(String categoryName, String categoryInformation, String productName, String productInformation, int price) {
+      super(categoryName, categoryInformation, productName, productInformation, price);
+   }
+}

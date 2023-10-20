06aadddfd3db175838dd95405a1e668e870fd157
junxtar <aa1870088@naver.com>
2023-10-20 16:28:25 +0900

    feat: 주문 모델 로직 구현

--- src/model/order/Order.java
+++ src/model/order/Order.java
@@ -1,13 +1,19 @@
-package model;
+package model.order;
+
+import model.product.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Order {
-   private static long id = 0;
-   private List<Product> products = new ArrayList<>();
 
-   public static long getId() {
+   private int id;
+   private final List<Product> products = new ArrayList<>();
+   private final HashMap<Product, Integer> productMap = new HashMap<>();
+   private OrderStatus orderStatus;
+
+   public int getId() {
       return id;
    }
 
@@ -15,10 +21,31 @@ public class Order {
       return products;
    }
 
+   public HashMap<Product, Integer> getProductMap() {
+      return productMap;
+   }
+
+   public String getOrderStatus() {
+      return orderStatus.getStatus();
+   }
+
+   public void setOrderStatus(OrderStatus orderStatus) {
+      this.orderStatus = orderStatus;
+   }
+
    public void addProduct(Product p) {
       this.products.add(p);
    }
-   public int getTotalPrice() {
-       return products.stream().mapToInt(Product::getPrice).sum();
+
+   public void addProductMap(Product product) {
+      productMap.put(product, productMap.getOrDefault(product, 0) + 1);
    }
+
+   public Order successOrder() {
+      System.out.println(getOrderStatus() + "주문이 완료되었습니다!");
+      System.out.println("대기 번호는 [ " + getId() + " ] 번 입니다.");
+      // thread? 3sec ? -> run();
+      return this;
+   }
+
 }

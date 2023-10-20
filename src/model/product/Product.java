4a1bf5390228cb1fd067f225ade025063b47b3a3
junxtar <aa1870088@naver.com>
2023-10-20 16:29:13 +0900

    feat: 상품 모델 구현

---
+++ src/model/product/Product.java
@@ -0,0 +1,36 @@
+package model.product;
+
+import model.category.Category;
+
+public class Product extends Category {
+
+   private final int productId;
+   private final String productName;
+   private final String productInformation;
+   private final int price;
+
+   public Product(int categoryId, String categoryName, String categoryInformation,int productId, String productName, String productInformation, int price) {
+      super(categoryId, categoryName, categoryInformation);
+      this.productId = productId;
+      this.productName = productName;
+      this.productInformation = productInformation;
+      this.price = price;
+   }
+
+   public int getProductId() {
+      return productId;
+   }
+
+   public String getProductName() {
+      return productName;
+   }
+
+   public String getProductInformation() {
+      return productInformation;
+   }
+
+   public int getPrice() {
+      return price;
+   }
+
+}

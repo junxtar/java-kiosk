1af0c64ce5d07fe8a919e065ffbd6a5fd211b3e7
junxtar <aa1870088@naver.com>
2023-10-20 16:12:58 +0900

    refactor: topping 예제 삭제

--- src/example/ExamCategory.java
+++ src/example/ExamCategory.java
@@ -1,14 +1,11 @@
 package example;
 
-import model.Category;
-import java.util.List;
+import model.category.Category;
 
 public class ExamCategory {
-   public final static Category PIZZA = new Category("Pizza", "쫄깃한 도우와 이탈리아 치즈 그리고 수제 소스의 환상적인 궁합");
-   public final static Category TOPPING = new Category("Topping", "베이컨, 치즈, 페퍼로니, 없는게 없어유 ~ ");
-   public final static Category BEVERAGE = new Category("Beverage", "주인장은 제로 음료도 도입을 했습니다.");
-   public final static Category SAUCE = new Category("Sauce", "소스를 곁들이면 피자 맛이 두배~");
-   public final static Category SIDE = new Category("Side", "피자로는 부족하다면 탁월한 선택!");
+   public final static Category PIZZA = new Category(1, "Pizza", "쫄깃한 도우와 이탈리아 치즈 그리고 수제 소스의 환상적인 궁합");
+   public final static Category BEVERAGE = new Category(2, "Beverage", "주인장은 제로 음료도 도입을 했습니다.");
+   public final static Category SAUCE = new Category(3, "Sauce", "소스를 곁들이면 피자 맛이 두배~");
+   public final static Category SIDE = new Category(4, "Side", "피자로는 부족하다면 탁월한 선택!");
 
-   public final static List<Category> CATEGORIES = List.of(PIZZA, TOPPING, BEVERAGE, SAUCE, SIDE);
 }
--- src/example/ExamProduct.java
+++ src/example/ExamProduct.java
@@ -1,11 +1,6 @@
 package example;
 
-import model.Beverage;
-import model.Pizza;
-import model.Product;
-import model.Sauce;
-import model.Side;
-import model.Topping;
+import model.product.Product;
 
 import java.util.List;
 
@@ -13,54 +8,45 @@ import static example.ExamCategory.BEVERAGE;
 import static example.ExamCategory.PIZZA;
 import static example.ExamCategory.SAUCE;
 import static example.ExamCategory.SIDE;
-import static example.ExamCategory.TOPPING;
 
 public class ExamProduct {
    // pizza
-   public final static Pizza PEPPERONI = new Pizza(PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
-           "PEPPERONI_PIZZA", "피자러버들의 함성소리가 들립니다. 무슨 말이 필요할까요", 18000);
-   public final static Pizza CHEESE = new Pizza(PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
-           "CHEESE_PIZZA", "토핑이 없어도 이렇게 맛있을 수 있구나. 감탄 준비하세요", 18000);
-   public final static Pizza HAWAIIAN = new Pizza(PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
-           "HAWAIIAN_PIZZA", "진지하게 말씀드리지만 맛있습니다.(내피셜)", 18000);
-   public final static Pizza SWEET_POTATO = new Pizza(PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
-           "SWEET_POTATO_PIZZA", "피자계의 입문코스이자, 한국의 3대장 피자 중 하나", 18000);
-   public final static Pizza BULGOGI = new Pizza(PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
-           "BULGOGI_PIZZA", "한국과 이탈리아의 최고의 콜라보!", 18000);
-   // topping
-   public final static Topping BACON = new Topping(TOPPING.getCategoryName(), TOPPING.getCategoryInformation(),
-           "BACON", "피자에 감칠맛과 짭쪼름함을 더하다", 2000);
-   public final static Topping POTATO = new Topping(TOPPING.getCategoryName(), TOPPING.getCategoryInformation(),
-           "POTATO", "피자에 고소함을 더하다", 2000);
-   public final static Topping MEAT = new Topping(TOPPING.getCategoryName(), TOPPING.getCategoryInformation(),
-           "MEAT", "피자의 허전함을 식감으로 채우다", 3000);
-   public final static Topping MUSHROOM = new Topping(TOPPING.getCategoryName(), TOPPING.getCategoryInformation(),
-           "MUSHROOM", "건강과 식감을 챙기고 싶다면 선택!", 1500);
+   public final static Product PEPPERONI = new Product(PIZZA.getCategoryId(), PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
+           1, "PEPPERONI_PIZZA", "피자러버들의 함성소리가 들립니다. 무슨 말이 필요할까요", 18000);
+   public final static Product CHEESE = new Product(PIZZA.getCategoryId(), PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
+           2, "CHEESE_PIZZA", "토핑이 없어도 이렇게 맛있을 수 있구나. 감탄 준비하세요", 18000);
+   public final static Product HAWAIIAN = new Product(PIZZA.getCategoryId(), PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
+           3, "HAWAIIAN_PIZZA", "진지하게 말씀드리지만 맛있습니다.(내피셜)", 18000);
+   public final static Product SWEET_POTATO = new Product(PIZZA.getCategoryId(), PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
+           4, "SWEET_POTATO_PIZZA", "피자계의 입문코스이자, 한국의 3대장 피자 중 하나", 18000);
+   public final static Product BULGOGI = new Product(PIZZA.getCategoryId(), PIZZA.getCategoryName(), PIZZA.getCategoryInformation(),
+           5, "BULGOGI_PIZZA", "한국과 이탈리아의 최고의 콜라보!", 18000);
+
    // beverage
-   public final static Beverage COKE = new Beverage(BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
-           "COKE", "코카콜라는 국룰 아시죠!?", 2000);
-   public final static Beverage ZERO_COKE = new Beverage(BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
-           "ZERO_COKE", "주인장은 다이어트 때문에 이걸 마십니다!", 2000);
-   public final static Beverage ICE_TEA = new Beverage(BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
-           "ICE_TEA", "달달한 복숭아~", 2500);
-   public final static Beverage AMERICANO = new Beverage(BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
-           "AMERICANO", "달콤 쌈싸름한 커피!", 1500);
+   public final static Product COKE = new Product(BEVERAGE.getCategoryId(), BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
+           1, "COKE", "코카콜라는 국룰 아시죠!?", 2000);
+   public final static Product ZERO_COKE = new Product(BEVERAGE.getCategoryId(), BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
+           2, "ZERO_COKE", "주인장은 다이어트 때문에 이걸 마십니다!", 2000);
+   public final static Product ICE_TEA = new Product(BEVERAGE.getCategoryId(), BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
+           3, "ICE_TEA", "달달한 복숭아~", 2500);
+   public final static Product AMERICANO = new Product(BEVERAGE.getCategoryId(), BEVERAGE.getCategoryName(), BEVERAGE.getCategoryInformation(),
+           4, "AMERICANO", "달콤 쌈싸름한 커피!", 1500);
    // sauce
-   public final static Sauce GARLIC_DIPPING = new Sauce(SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
-           "GARLIC_DIPPING", "도우에 찍어먹으면 찰떡궁합", 500);
-   public final static Sauce CHILI = new Sauce(SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
-           "CHILI", "새콤 달콤 매콤 3개의 조합", 500);
-   public final static Sauce HOT = new Sauce(SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
-           "HOT", "매콤함으로 새로운 맛을 내봐요!", 500);
-   public final static Sauce MUSTARD = new Sauce(SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
-           "MUSTARD", "겨자씨를 이용해 달콤함을 구현한 소스!", 500);
+   public final static Product GARLIC_DIPPING = new Product(SAUCE.getCategoryId(), SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
+           1, "GARLIC_DIPPING", "도우에 찍어먹으면 찰떡궁합", 500);
+   public final static Product CHILI = new Product(SAUCE.getCategoryId(), SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
+           2, "CHILI", "새콤 달콤 매콤 3개의 조합", 500);
+   public final static Product HOT = new Product(SAUCE.getCategoryId(), SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
+           3, "HOT", "매콤함으로 새로운 맛을 내봐요!", 500);
+   public final static Product MUSTARD = new Product(SAUCE.getCategoryId(), SAUCE.getCategoryName(), SAUCE.getCategoryInformation(),
+           4, "MUSTARD", "겨자씨를 이용해 달콤함을 구현한 소스!", 500);
    // side
-   public final static Side SPAGHETTI = new Side(SIDE.getCategoryName(), SIDE.getCategoryInformation(),
-           "SPAGHETTI", "피자와 스파게티는 한몸입니다.", 6500);
-   public final static Side BUFFALO_WINGS = new Side(SIDE.getCategoryName(), SIDE.getCategoryInformation(),
-           "BUFFALO_WINGS(5EA)", "저희 가게는 수제 버팔로 윙을 직접 사용합니다.", 5500);
-   public final static Side CHEESE_BALL = new Side(SIDE.getCategoryName(), SIDE.getCategoryInformation(),
-           "CHEESE_BALL(5EA)", "치즈가 쭈우우욱 늘어나유~", 4500);
+   public final static Product SPAGHETTI = new Product(SIDE.getCategoryId(), SIDE.getCategoryName(), SIDE.getCategoryInformation(),
+           1, "SPAGHETTI", "피자와 스파게티는 한몸입니다.", 6500);
+   public final static Product BUFFALO_WINGS = new Product(SIDE.getCategoryId(), SIDE.getCategoryName(), SIDE.getCategoryInformation(),
+           2, "BUFFALO_WINGS(5EA)", "저희 가게는 수제 버팔로 윙을 직접 사용합니다.", 5500);
+   public final static Product CHEESE_BALL = new Product(SIDE.getCategoryId(), SIDE.getCategoryName(), SIDE.getCategoryInformation(),
+           3, "CHEESE_BALL(5EA)", "치즈가 쭈우우욱 늘어나유~", 4500);
 
    public final static List<Product> PRODUCTS = List.of(
            PEPPERONI,
@@ -68,10 +54,6 @@ public class ExamProduct {
            HAWAIIAN,
            SWEET_POTATO,
            BULGOGI,
-           BACON,
-           POTATO,
-           MEAT,
-           MUSHROOM,
            COKE,
            ZERO_COKE,
            ICE_TEA,

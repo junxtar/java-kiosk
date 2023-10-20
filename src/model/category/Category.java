2d82933756922a39657fa42c7674ac0500952e0b
junxtar <aa1870088@naver.com>
2023-10-20 16:28:00 +0900

    feat: 카테고리 모델 구현

---
+++ src/model/category/Category.java
@@ -0,0 +1,27 @@
+package model.category;
+
+public class Category {
+
+   private final int categoryId;
+   private final String categoryName;
+   private final String categoryInformation;
+
+   public Category(int id, String categoryName, String categoryInformation) {
+      this.categoryId = id;
+      this.categoryName = categoryName;
+      this.categoryInformation = categoryInformation;
+   }
+
+   public int getCategoryId() {
+      return categoryId;
+   }
+
+   public String getCategoryName() {
+      return categoryName;
+   }
+
+   public String getCategoryInformation() {
+      return categoryInformation;
+   }
+
+}

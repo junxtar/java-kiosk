bf52b3aa19c1b0e352ccb2806e14eaa71bd99341
junxtar <aa1870088@naver.com>
2023-10-28 15:24:47 +0900

    feat: 입력값 검증 처리 공통 로직 구현

--- src/util/context/UtilContext.java
+++ src/util/context/UtilContext.java
@@ -7,4 +7,13 @@ public class UtilContext {
    public static void line() {
       System.out.println("--------------------------------------------------------");
    }
+   public static  int validInput(int minValue, int maxValue, int input) {
+      while (input < minValue || input > maxValue) {
+         line();
+         System.out.println("[ 잘못된 입력입니다. ]");
+         System.out.println("  다시 입력 바랍니다.: ");
+         input = sc.nextInt();
+      }
+      return input;
+   }
 }

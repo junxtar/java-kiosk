3c3741ff3dabdd6ae0e717fb6af2f28b81d3cd52
junxtar <aa1870088@naver.com>
2023-10-28 15:21:47 +0900

    feat: KioskController 구현

--- src/kiosk/controller/KioskController.java
+++ src/kiosk/controller/KioskController.java
@@ -1,39 +1,26 @@
 package kiosk.controller;
 
 import kiosk.service.KioskService;
-import order.model.Order;
-import product.model.Product;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class KioskController {
 
-   private final List<Product> products = new ArrayList<>();
    KioskService kioskService = new KioskService();
+
    public void run() throws InterruptedException, NoSuchAlgorithmException {
-      helloMessage();
-      if (isGuest()) {
-         guestMode();
+      kioskService.helloMessage();
+      if (kioskService.isGuest()) {
+         kioskService.guestMode();
+         run();
       } else {
-         if (passwordCheck() && managerMode(products)) {
+         if (kioskService.passwordCheck()) {
+            kioskService.managerMode();
             run();
          } else {
-            passwordNotMatchMessage();
+            kioskService.passwordNotMatchMessage();
             run();
          }
       }
-   }
-   public void helloMessage() {
-      System.out.println("Junxtar Pizza 에 오신걸 환영합니다!");
-   }
-
-   private void guestMode() throws InterruptedException, NoSuchAlgorithmException {
-      Order order = orderService.initOrder();
-      order.setOrderStatus(isTakeOut());
-      products.addAll(ordering(order).getProducts());
-      successOrder(order);
-      run();
    }
 }

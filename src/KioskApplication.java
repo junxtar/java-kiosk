2a6cf62c3d0861afecceb6a5cc630bec8c5c47ad
junxtar <aa1870088@naver.com>
2023-10-28 15:25:25 +0900

    clean: Application 코드 개선

--- src/KioskApplication.java
+++ src/KioskApplication.java
@@ -4,7 +4,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class KioskApplication {
    public static void main(String[] args) throws InterruptedException, NoSuchAlgorithmException {
-      KioskController kisok = new KioskController();
-      kisok.run();
+      KioskController kioskController = new KioskController();
+      kioskController.run();
    }
 }
